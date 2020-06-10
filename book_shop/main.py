cebfffc4fdf187301c5d1a0fde589cd304e95c4e
Tim Sokollek <tim.sokollek@gmail.com>
2020-06-10 18:07:01 +0200

    First implementation of get_total_price, test failing

--- book_shop/main.py
+++ book_shop/main.py
@@ -1,5 +1,5 @@
 from enum import Enum
-from typing import Sequence
+from typing import List
 
 
 class Volume(Enum):
@@ -11,13 +11,38 @@ class Volume(Enum):
 
 
 class BookInstance(object):
+    price = 8
+
     def __init__(self, volume: Volume) -> None:
         self.volume = volume
 
 
 class Basket(object):
-    def __init__(self, items: Sequence[BookInstance] = []) -> None:
+    discounts = {2: 0.05, 3: 0.10, 4: 0.20, 5: 0.25}
+
+    def __init__(self, items: List[BookInstance] = []) -> None:
         self.items = items
 
-    def get_total_price(self) -> int:
-        return 0
+    def get_total_price(self) -> float:
+        different_titles:List[BookInstance] = []
+        duplicate_titles:List[BookInstance] = []
+
+        for book_instance in self.items:
+            if book_instance.volume not in [book.volume for book in different_titles]:
+                different_titles.append(book_instance)
+            else:
+                duplicate_titles.append(book_instance)
+
+        amount_of_different_titles = len(different_titles)
+        discount_for_different_titles = Basket.discounts.get(
+            amount_of_different_titles, 0
+        )
+        different_titles_price = sum([book.price for book in different_titles])
+        different_titles_price_discounted = different_titles_price * (
+            1 - discount_for_different_titles
+        )
+        duplicate_titles_price = sum([book.price for book in duplicate_titles])
+
+        total_price = different_titles_price_discounted + duplicate_titles_price
+
+        return total_price
